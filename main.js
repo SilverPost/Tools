1eeb2c5fe53e1dd8bf760f217eda388a0d891337
SilverPost <object.trade.sort+100@gmail.com>
2018-07-06 22:45:40 +0900

    [alphaScaleCsvCreater] Add alphaScaleCsvCreater.

---
+++ main.js
@@ -0,0 +1,26 @@
+function csv() {
+  var nw_lat = Number(document.js.nw_lat.value);
+  var nw_long = Number(document.js.nw_long.value);
+  var se_lat = Number(document.js.se_lat.value);
+  var se_long = Number(document.js.se_long.value);
+  var alpha_0_5 = Number(document.js.alpha_0_5.value);
+  var alpha_eew = Number(document.js.alpha_eew.value);
+  
+  var div = document.getElementById("csvText");
+  for(var lat = nw_lat; lat <= se_lat; lat += 0.25) {
+    for(var long = nw_long; long <= se_long; long += 0.25) {
+      for(var depth = 0; depth <= 100; depth += 20) {
+        div.innerHTML = div.innerHTML+lat+","+long+","+depth+","+alpha_0_5+","+alpha_eew+"<br>";
+      }
+    }
+  }
+}
+
+function clr() {
+  document.js.nw_lat.value="";
+  document.js.nw_long.value="";
+  document.js.se_lat.value="";
+  document.js.se_long.value="";
+  document.js.alpha_0_5.value="";
+  document.js.alpha_eew.value="";
+}
