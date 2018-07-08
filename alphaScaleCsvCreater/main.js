6e33ebaaef4c7072ada7f6366cfe5b14af994201
SilverPost <object.trade.sort+100@gmail.com>
2018-07-08 19:36:20 +0900

    [alphaScaleCsvCreater] A csv file is saved when the link is clicked.

--- alphaScaleCsvCreater/main.js
+++ alphaScaleCsvCreater/main.js
@@ -1,3 +1,6 @@
+var CSV_DATA4DISPLAY = "";
+var CSV_DATA4SAVE = "";
+
 function csvAdd() {
   var nw_lat = Number(document.js.nw_lat.value);
   var nw_long = Number(document.js.nw_long.value);
@@ -6,19 +9,26 @@ function csvAdd() {
   var alpha_0_5 = Number(document.js.alpha_0_5.value);
   var alpha_eew = Number(document.js.alpha_eew.value);
   
-  var div = document.getElementById("csvText");
   for(var lat = nw_lat; lat >= se_lat; lat -= 0.25) {
     for(var long = nw_long; long <= se_long; long += 0.25) {
       for(var depth = 0; depth <= 100; depth += 20) {
-        div.innerHTML = div.innerHTML+lat+","+long+","+depth+","+alpha_0_5+","+alpha_eew+"<br>";
+        CSV_DATA4DISPLAY = CSV_DATA4DISPLAY+lat+","+long+","+depth+","+alpha_0_5+","+alpha_eew+"<br>";
+        CSV_DATA4SAVE = CSV_DATA4SAVE+lat+","+long+","+depth+","+alpha_0_5+","+alpha_eew+"\n";
       }
     }
   }
+  updateInnerHtml(CSV_DATA4DISPLAY);
 }
 
 function csvInit() {
+  CSV_DATA4DISPLAY = "";
+  updateInnerHtml(CSV_DATA4DISPLAY);
+  CSV_DATA4SAVE = "";
+}
+
+function updateInnerHtml(text) {
   var div = document.getElementById("csvText");
-  div.innerHTML = "";
+  div.innerHTML = text;
 }
 
 function clr() {
@@ -28,4 +38,19 @@ function clr() {
   document.js.se_long.value="";
   document.js.alpha_0_5.value="";
   document.js.alpha_eew.value="";
+}
+
+function csvSave() {
+  outputText(CSV_DATA4SAVE, 'alpha');
+}
+
+function outputText(text, fileName) {
+  var blob = new Blob([ text ], {"type": "text/csv"});
+  if (window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(blob, fileName);
+    window.navigator.msSaveOrOpenBlob(blob, fileName);
+  } else {
+    document.getElementById("download").href = window.URL.createObjectURL(blob);
+    document.getElementById("download").download = fileName;
+  }
 }
